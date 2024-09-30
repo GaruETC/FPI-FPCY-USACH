311a8836113f9a7a06ddd06a5a12f0258aa1a0f6
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2024-09-29 21:19:16 -0300

    feat(tests_funcionales): add comparison between the expected data of test file and the actual output of the python file

--- Material_Complementario/Utilidades/tests_funcionales/main.py
+++ Material_Complementario/Utilidades/tests_funcionales/main.py
@@ -1,10 +1,35 @@
 from utils import cli
-#import os, sys, subprocess
+import os, sys, subprocess
 
 prog_info = cli.get_args()
 
-# Test getter
+# Getter
 test = prog_info.get_test()
 
-print(f"Input: {test.input}")
-print(f"Output: {test.output}")
+# Runner
+prog_command = [sys.executable, prog_info.file_path]
+prog_test_time = 5
+prog_process = subprocess.Popen(
+    prog_command,
+    stdin   = subprocess.PIPE,
+    stdout  = subprocess.PIPE,
+    stderr  = subprocess.PIPE,
+    text    = True,
+    cwd     = os.path.dirname(prog_info.file_path)
+)
+prog_input  = '\n'.join(test.input)
+prog_output  = '\n'.join(test.output)
+prog_stdout, prog_stderr = prog_process.communicate(input=prog_input, timeout=prog_test_time)
+prog_exit_code= prog_process.returncode
+
+
+if prog_stderr:
+    print('Error: ', prog_stderr)
+elif prog_stdout[:-1] == prog_output:
+    print("[✓] Test Correcto")
+else:
+    print("[X] Test Incorrecto")
+    print(f'- Entrada del Programa: \n"""\n{prog_input}\n"""')
+    print(f'- Salida del Programa:\n"""\n{prog_stdout}\n"""')
+    print(f'- Salida Esperada \n"""\n{prog_output}\n"""')
+    print("\n\n")
